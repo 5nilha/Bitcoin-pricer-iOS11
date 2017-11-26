f4ab780f38ee0cb8d67d719976bc0110c2e913c3
5nilha <bwtdirect@gmail.com>
2017-11-26 01:46:57 -0500

    Last commit

---
+++ BitcoinTicker/ViewController.swift
@@ -0,0 +1,126 @@
+//
+//  ViewController.swift
+//  BitcoinTicker
+//
+//  Created by Angela Yu on 23/01/2016.
+//  Copyright © 2016 London App Brewery. All rights reserved.
+//
+
+import UIKit
+import SwiftyJSON
+import Alamofire
+
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+   
+    
+    
+    let baseURL = "https://apiv2.bitcoinaverage.com/indices/global/ticker/BTC"
+    let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
+    let currencySymbol = ["$", "R$", "$", "¥", "€", "£", "$", "Rp", "₪", "₹", "¥", "$", "kr", "$", "zł", "lei", "₽", "kr", "$", "$", "R"]
+   
+    var currencySymbolIndex = 0;
+    var finalURL = ""
+
+    //Pre-setup IBOutlets
+    @IBOutlet weak var bitcoinPriceLabel: UILabel!
+    @IBOutlet weak var currencyPicker: UIPickerView!
+    
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+        currencyPicker.dataSource = self
+        currencyPicker.delegate = self
+        
+    }
+
+    
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return currencyArray.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return currencyArray[row]
+    }
+    
+    
+    
+    //Changes the font color in the pickerview
+    func pickerView(_ pickerView: UIPickerView, attributedTitleForRow row: Int, forComponent component: Int) -> NSAttributedString? {
+        
+        let title = currencyArray[row]
+        
+        //Create an attribute with an attributes array
+        let attributes = NSAttributedString(string: title, attributes: [NSAttributedStringKey.foregroundColor: UIColor.white, NSAttributedStringKey.font: UIFont(name: "Avenir Book", size: 28)!])
+        
+        return attributes
+    }
+    
+
+    
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        finalURL = baseURL + currencyArray[row]
+        currencySymbolIndex = row
+        print(finalURL)
+        getBitcoinData(url: finalURL)
+    }
+
+    
+    
+    
+    
+    
+    //MARK: - Networking
+    /***************************************************************/
+    
+    func getBitcoinData(url: String) {
+        
+        Alamofire.request(url, method: .get)
+            .responseJSON { response in
+                if response.result.isSuccess {
+
+                    print("Success! Got the Bitcoin data")
+                    let bitCoinJSON : JSON = JSON(response.result.value!)
+
+                    self.updateBitCoinData(json: bitCoinJSON)
+
+                } else {
+                    print("Error: \(String(describing: response.result.error))")
+                    self.bitcoinPriceLabel.text = "Connection Issues"
+                }
+            }
+
+    }
+
+    
+    
+    
+    
+    //MARK: - JSON Parsing
+    /***************************************************************/
+    
+    func updateBitCoinData(json : JSON) {
+        
+        if let bitCoinResult = json["ask"].double {
+        
+        bitcoinPriceLabel.text = "\(currencySymbol[currencySymbolIndex])\(bitCoinResult)"
+            
+        }
+        else {
+            bitcoinPriceLabel.text = "Price unavailable"
+        }
+    }
+    
+
+
+
+
+}
+
